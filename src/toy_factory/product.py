4b432198641746bb008a5a2342a309b0f6c5d7ac
Mouloud BELLIL <mouloud.bellil@outlook.fr>
2026-01-23 12:28:40 +0100

    ajout de petites mises à jour dans class Product

--- src/toy_factory/product.py
+++ src/toy_factory/product.py
@@ -2,7 +2,6 @@ from datetime import datetime
 import logging
 import random
 import re
-import time
 from typing import Dict, List
 
    
@@ -19,20 +18,25 @@ class Product:
 
     def __init__(self, logger: logging.Logger) -> None:
         Product.SERIAL_NUMBER += 1
+        self.logger = logger
         self._product_color = random.choice(Product.COLORS)
         self._status = Product.STATUS_IN_CREATED
         self._production_time = .0
-        self.logger = logger
         self._id = f"TOY-{Product.SERIAL_NUMBER:03d}-{self._product_color.upper()}"
         self._history: List[Dict] = []
         
-        self.logger.info(f"Début de fabrication du produit {self._id}.")
+        self.logger.info(f"Creation du produit {self._id}.")
 
     def __str__(self) -> str:
-        return f"{self._id} (Status: {self._status} - Times: {self._production_time}s)"
+        return f"{self._id} (Status: {self._status} - Production-time: {self._production_time}s)"
     
     def __repr__(self) -> str:
-        return f"Product(id={self._id}, status={self._status})"
+        return f"Product(id={self._id}, status={self._status}, Production-time={self._production_time})"
+    
+    def _update_production_time(self, duration: float) -> float:
+        """Mettre à jour le temps total de production"""
+        self._production_time += duration
+        return self._production_time
     
     def get_status(self) -> None:
         return self._status
@@ -63,41 +67,43 @@ class Product:
         self._status = Product.STATUS_IN_FINISHED
         self.logger.info(f"Produit {self._id} marqué comme fini en {self._production_time}s")
     
-    def _update_production_time(self, duration: float) -> float:
-        """Mettre à jour le temps total de production"""
-        self._production_time += duration
-        return self._production_time
-    
     def add_production_step(self, station_name: str, duration: float) -> None:
         """Ajoute une étape de production à l"historique"""
         step = {
-            "timestamp": datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S"),
             "station": station_name,
+            "start_date": datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S"),
             "duration": duration,
-            "production_time": self._update_production_time(duration)
         }
         self._history.append(step)
-        self.logger.info(f"Produit {self._id} a terminé l'étape {station_name.upper()} en {duration}s.")
+        self._update_production_time(duration)
+        self.logger.info(f"Produit {self._id} a terminé l'étape '{station_name}' en {duration}s.")
 
 
 if __name__ == "__main__":
     from utils.logger import config_logging
     
     logger = config_logging()
+    print("Initialisation du produit :")
     p1 = Product(logger)
+    print("\nAffichger pour l'utilisateur via la méthode __str__ :")
     print(p1)
+    print("\nAffichger pour le développeur via la méthode __repr__ :")
     print(p1.__repr__())
-    print(f"Details du produit :")
+    print(f"\nDetails du produit :")
     print("     Status :", p1.get_status())
     print("     ID :", p1.get_id())
     print("     Numéro de série :", p1.get_serial_number())
     print("     Historique :", p1.get_history())
-    print("     Format de l'ID :", p1.validate_id())
+    print("     Format de l'ID ? :", p1.validate_id())
+    print("\nProduit en cours de traitement :")
     p1.mark_in_progress()
+    print("\nProduit déféctueux :")
     p1.mark_defective()
+    print("\nProduit fini :")
     p1.mark_finished()
+    print("\nStations :")
     p1.add_production_step("Assemblage", 2.3)
     p1.add_production_step("Peinture", 2.3)
-    print("Historique :" )
+    print("\nHistorique :" )
     print(p1.get_history())
     
